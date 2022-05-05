d3544c69b16b6c447ac90f52e55c7b8cbcc9d983
Ze <josericardodainese@gmail.com>
2022-05-05 09:39:20 -0300

    Start

---
+++ src/main/java/com/dainese/ricardo/jose/Java2JSON/Java2JsonApplication.java
@@ -0,0 +1,13 @@
+package com.dainese.ricardo.jose.Java2JSON;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java2JsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java2JsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dainese/ricardo/jose/Java2JSON/Java2JsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dainese.ricardo.jose.Java2JSON;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java2JsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
